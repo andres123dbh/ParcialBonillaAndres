b11c3e9bf6a776b29434d9056740b587f2b87c77
unknown <admin@BGAI202-11.bga.upb>
2020-09-04 18:11:21 -0500

    formulario inicial

---
+++ Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParcialBonillaAndres
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text == "admin" && txtPassword.Password == "1234")
+            {
+                MainWindow w = (MainWindow)Window.GetWindow(this);
+                w.frameMain.NavigationService.Navigate(new ValorVentas());
+            }
+            else
+            {
+                if (txtUsername.Text == "empleada" && txtPassword.Password == "4321")
+                {
+                    MainWindow w = (MainWindow)Window.GetWindow(this);
+                    w.frameMain.NavigationService.Navigate(new Panes());
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o Contraseña Incorrecta");
+                }
+            }
+        }
+    }
+}
