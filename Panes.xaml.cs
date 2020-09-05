0352d2f14fc8ed736ebcae61519149b3bec07365
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-09-04 21:06:12 -0500

    Funcionalidad y operaciones del formulario

--- Panes.xaml.cs
+++ Panes.xaml.cs
@@ -22,5 +22,56 @@ namespace ParcialBonillaAndres
         {
             InitializeComponent();
         }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string cantidad = txtCantidad.Text;
+            int cantidadPanes = Int32.Parse(cantidad);
+            string nombreClientes = txtNombre.Text;
+            string cedulaCliente = txtCedula.Text;
+            var item = (ComboBoxItem)tipoPan.SelectedValue;
+            var pan = (string)item.Content;
+            var valorPan = cantidadPanes;
+            DateTime dia = DateTime.Today;
+            var hora = DateTime.Now.ToString("hh:mm:ss");
+            if (pan == "Aliñado")
+            {
+                 valorPan = cantidadPanes * 1000;
+            }
+            else
+            {
+                if(pan == "No Aliñado")
+                {
+                     valorPan = cantidadPanes * 500;
+                }
+                else
+                {
+                    if(pan == "Espesial")
+                    {
+                         valorPan = cantidadPanes * 2000;
+                    }
+                }
+            }
+            
+            var add = "Fecha: " + dia.ToString("d") + "     Hora: " + hora        
+                + "\nPan: " + pan  + "     Cantidad de Panes: " + cantidadPanes + "\nTotal: "
+                + valorPan + "\nNombre del Cliente: " + nombreClientes
+                + "\nCedula del Cliente: " + cedulaCliente;
+            txtVentas.Inlines.Add(new Run(add));
+            txtVentas.Inlines.Add(new LineBreak());
+            txtVentas.Inlines.Add(new Run("--------------------------------------------------"));
+            txtVentas.Inlines.Add(new LineBreak());
+        }
+
+        private void btnlimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            txtVentas.Text = string.Empty;
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow w = (MainWindow)Window.GetWindow(this);
+            w.frameMain.NavigationService.Navigate(new Login());
+        }
     }
 }
